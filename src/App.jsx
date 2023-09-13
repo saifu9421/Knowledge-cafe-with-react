99773dce605cedc67d382d92cbf802e2026c878c
saifu9421 <saifursaif905@gmail.com>
2023-09-13 19:24:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+     
+
+     <h1 className='text-5xl bg-red-300'>
+      Knowldge cafe
+     </h1>
+
+    </>
+  )
+}
+
+export default App
